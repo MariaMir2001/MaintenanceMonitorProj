78eb7f698d1482e6e9fa1267ff4c5eb9df524e67
MariaMir2001 <wi21b105@technikum-wien.at>
2022-06-24 11:02:41 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenancemonitorproj/MaintenanceMonitorProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenancemonitorproj/MaintenanceMonitorProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
